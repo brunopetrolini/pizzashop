c2a36f4b3669ab2f16c7b221dacf9b9886914da7
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-28 20:48:34 -0300

    style: eslint initialization

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
 }
