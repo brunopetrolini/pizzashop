6e3c32ab05c268bb1846b9c60103900d1ab242af
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-30 20:18:56 -0300

    feat: register a new restaurant

--- src/api/sign-in.ts
+++ src/api/sign-in.ts
@@ -1,13 +1,9 @@
-import { z } from 'zod'
-
 import { api } from '@/lib/axios'
 
-const signInSchema = z.object({
-  email: z.string().email(),
-})
+export interface SignInBody {
+  email: string
+}
 
-export type SignInPayload = z.infer<typeof signInSchema>
-
-export async function signIn(payload: SignInPayload) {
-  await api.post('/authenticate', payload)
+export async function signIn(data: SignInBody) {
+  await api.post('/authenticate', data)
 }
